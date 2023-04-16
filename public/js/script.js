6b48650fe250366803c5ddc92443557a4b123265
Daghan Kurtay Altunsoy <daghan.altunsoy@gmail.com>
2023-04-16 22:46:45 +0300

    first commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,22 @@
-// Show a spinner on payment processing
-function setLoading(isLoading) {
+document.addEventListener("DOMContentLoaded", () => {
+  const bayBtn = document.querySelectorAll('button.btn-pay');
+  for (let i = 0; i < bayBtn.length; i++) {
+    bayBtn[i].addEventListener("click", function(e) {
+      e.preventDefault();
+      setLoading(e, true)
+    }, false);
+  }  
+});
+
+function setLoading(e, isLoading) {
+  console.log(e.target); 
   if (isLoading) {
-      // Disable the button and show a spinner
-      payBtn.disabled = true;
-      document.querySelector("#spinner").classList.remove("hidden");
-      document.querySelector("#buttonText").classList.add("hidden");
+    e.target.setAttribute('disabled', 'disabled');
+    e.target.parentNode.querySelector("div.loader").classList.remove("hide");
+    e.target.parentNode.querySelector("span.btn-text").classList.add("hide");
   } else {
-      // Enable the button and hide spinner
-      payBtn.disabled = false;
-      document.querySelector("#spinner").classList.add("hidden");
-      document.querySelector("#buttonText").classList.remove("hidden");
+    this.removeAttribute("disabled")
+    e.target.parentNode.querySelector("div.loader").classList.add("hide");
+    e.target.parentNode.querySelector("span.btn-text").classList.remove("hide");
   }
 }
